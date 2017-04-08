96076de03ce26d35c06019bcd1a5119f877259f7
GaganTut <gstut@live.com>
2017-04-07 16:50:06 -1000

    Passed default parameters which are cool

--- test/es6Practice.spec.js
+++ test/es6Practice.spec.js
@@ -323,20 +323,20 @@ describe('destructuring arrays makes shorter code', () => {
 // Default parameters - basics
 describe('default parameters make function parameters more flexible', () => {
   it('define it using an assignment to the parameter `function(param=1){}`', function() {
-    let number = (int) => int;
+    let number = (int = 0) => int;
 
     assert.equal(number(), 0);
   });
 
   it('it is used when undefined is passed', function() {
     let number = (int = 23) => int;
-    const param = 42;
+    const param = undefined;
 
     assert.equal(number(param), 23);
   });
 
   it('it is not used when a value is given', function() {
-    function xhr() {
+    function xhr(method = "Hello") {
       return method;
     }
 
@@ -344,7 +344,7 @@ describe('default parameters make function parameters more flexible', () => {
   });
 
   it('it is evaluated at run time', function() {
-    let defaultValue;
+    let defaultValue = 42;
     function xhr(method = `value: ${defaultValue}`) {
       return method;
     }
@@ -354,11 +354,10 @@ describe('default parameters make function parameters more flexible', () => {
   });
 
   it('it can also be a function', function() {
-    let defaultValue;
+    let defaultValue = () => {};
     function fn(value = defaultValue()) {
       return value;
     }
-
     assert.equal(fn(), defaultValue());
   });
 });
